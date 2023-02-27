12842813dd52bdd85559f2b4b5c7c7d4bf38f3e3
Alejandro <faustoaguanor@gmail.com>
2023-02-26 20:51:19 -0500

    Cambio modelo

--- dat-gui_panel.js
+++ dat-gui_panel.js
@@ -128,7 +128,7 @@ Q3D.gui.dat = {
     });
 
     // Plane altitude
-    this.customPlaneFolder.add(params.cp, 'd').min(zMin).max(zMax).name('Altitud').onChange(function (value) {
+    this.customPlaneFolder.add(params.cp, 'd').min(zMin).max(zMax).name('Altitude').onChange(function (value) {
       if (_this.customPlane === undefined) addPlane(params.cp.c);
       _this.customPlane.position.z = value * p.zScale;
       _this.customPlane.updateMatrixWorld();
